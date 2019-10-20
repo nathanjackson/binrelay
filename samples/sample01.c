bc696eb7167ccd0037d07232220424d6c5c772f1
Nathan Jackson <nathan@csnate.com>
2019-10-20 18:28:39 -0400

    prototyping reachability analysis

---
+++ samples/sample01.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include <pthread.h>
+
+static int x;
+
+void *t1_body()
+{
+    x = 5;
+}
+
+void *t2_body()
+{
+    x = 7;
+}
+
+int main(int argc, char **argv)
+{
+    pthread_t t1;
+    pthread_t t2;
+
+    x = 3;
+
+    pthread_create(&t1, NULL, t1_body, NULL);
+    pthread_create(&t2, NULL, t2_body, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("x=%d\n", x);
+
+    return 0;
+}
